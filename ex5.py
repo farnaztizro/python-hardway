005c9ff5c44c5cde993cbc77982b35581a3e5eb6
farnaz tizrou <far.tizro@gmail.com>
2020-11-20 21:49:40 +0330

    exercise5

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = 'farnaz'
+my_age = 21
+my_weight = 62 #kg
+my_height = 168 #cm
+my_hair = 'light'
+my_eyes = 'light brown'
+
+print(f"let's talk about {my_name}.")
+print(f"she's {my_weight} kilogram heavy.")
+print(f"she's {my_height} centimeters tall.")
+print(f"she's got {my_eyes} eyes and {my_hair} hair.")
+
+#this is optional 
+total = my_age + my_height + my_weight
+print("we have total of my age ,my weight and my height=",total)
