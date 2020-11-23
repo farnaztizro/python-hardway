585078d61426854d3005c74c5bd70dc46d043234
farnaz tizrou <far.tizro@gmail.com>
2020-11-23 13:11:21 +0330

    exercise 11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print("how old r u?", end='')
+age = input()
+
+print("how tall are u?", end='')
+height = input()
+
+print("how much do u weight?", end='')
+weight = input()
+
+print(f"so, you're {age} old, {height} cm tall and {weight} kg heavy.")
