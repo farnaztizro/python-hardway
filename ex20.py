b6e6314591f2e11f15d453278a002ed6235a0d25
farnaz tizro <far.tizro@gmail.com>
2020-11-27 18:38:09 +0330

    Functions and Files

---
+++ ex20.py
@@ -0,0 +1,40 @@
+from sys import argv
+script, input_file = argv
+
+def print_all(f):
+    print(f.read())
+
+def rewind(f):
+    f.seek(0)    
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline())  
+
+current_file = open(input_file)
+
+print("First let's print the whole file:\n")
+print_all(current_file)
+
+print("let's rewind")
+rewind(current_file)
+
+print("let's print three lines:")
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+current_line = current_line + 1
+print_a_line(current_line, current_file)
+
+#First let's print the whole file:
+#This is line 1
+#This is line 2
+#This is line 3
+#let's rewind
+#let's print three lines:
+#1 This is line 1
+#2 This is line 2
+#3 This is line 3
