817dcb9df5f824b3f01f103a0577230e1e8d8489
farnaz tizro <far.tizro@gmail.com>
2020-12-20 22:26:57 +0330

    Basic Object-Oriented Analysis and Design

---
+++ ex43.py
@@ -0,0 +1,25 @@
+from sys import exit 
+from random import randint
+# function dedent: write our room descriptions using triple-quote strings
+from textwrap import dedent 
+
+class Scene(object):
+
+    def enter(self):
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter()")
+        exit(1)
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished') 
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+            
